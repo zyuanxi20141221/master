6f6b7b68c03b0390fbdddd0f86d8d1cd2f3b36b1
zyuanxi20141221 <johnzheng2016@163.com>
2018-07-14 18:12:03 +0800

    fist commit

--- app/src/main/java/com/xzxx/decorate/o2o/master/MainActivity.java
+++ app/src/main/java/com/xzxx/decorate/o2o/master/MainActivity.java
@@ -3,6 +3,8 @@ package com.xzxx.decorate.o2o.master;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.xzxx.decorate.o2o.master.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
